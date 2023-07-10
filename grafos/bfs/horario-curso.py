9b80d9d294ffdf421a8409f935ccec2e67e56bb9
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-07-10 08:26:45 -0500

    Ejercicios con bfs

---
+++ grafos/bfs/horario-curso.py
@@ -0,0 +1,35 @@
+# Hay un total de numCursos cursos que tiene que tomar, etiquetados de 0 a numCursos - 1. Se le da una matriz prerrequisitos donde prerrequisitos[i] = [ai, bi] indica que debe tomar el curso bi primero si quiere tomar el curso ai. Por ejemplo, el par [0, 1], indica que para tomar el curso 0 hay que tomar primero el curso 1. Devuelve true si puedes terminar todos los cursos. En caso contrario, devuelve false.
+
+# Ejemplo 1:
+# Entrada: numCursos = 2, prerrequisitos = [[1,0]]
+# Salida: true
+# Explicación: Hay un total de 2 cursos para tomar. Para tomar el curso 1 debes haber terminado el curso 0. Así que es posible.
+
+from collections import defaultdict, deque
+
+
+class Node:
+    def __init__(self):
+        self.indegree = 0
+        self.outNodes = []
+
+
+class Solution:
+    def canFinish(self, numCourses, prerequisitos):
+        grafo = defaultdict(Node)
+        for cursos in prerequisitos:
+            grafo[cursos[0]].indegree += 1
+            grafo[cursos[1]].outNodes.append(grafo[cursos[0]])
+        cola = deque()
+        for course in grafo:
+            if grafo[course].indegree == 0:
+                cola.append(grafo[course])
+        removerConexion = 0
+        while cola:
+            course = cola.popleft()
+            for nextCourse in course.outNodes:
+                removerConexion += 1
+                nextCourse.indegree -= 1
+                if nextCourse.indegree == 0:
+                    cola.append(nextCourse)
+        return removerConexion == len(prerequisitos)
---
+++ grafos/bfs/vuelo-mas-barato-k-paradas.py
@@ -0,0 +1,40 @@
+# Hay n ciudades conectadas por un cierto número de vuelos. Se le da un array flights donde flights[i] = [fromi, toi, pricei] indica que hay un vuelo desde la ciudad fromi a la ciudad toi con un precioi. También se le dan tres enteros src, dst y k, devuelve el precio más barato de src a dst con un máximo de k paradas. Si no existe tal ruta, devuelve -1.
+
+# Ejemplo 1:
+# Entrada:
+# n = 4
+# vuelos = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]]
+# src = 0
+# dst = 3
+# k = 1
+# Salida: 700
+
+# Explicación:El gráfico se muestra arriba. El camino óptimo, con un máximo de 1 parada, desde la ciudad 0 a la 3 está marcado en rojo y tiene un coste de 100 + 600 = 700. Nótese que el camino que pasa por las ciudades [0,1,2,3] es más barato pero no es válido porque utiliza 2 paradas.
+
+from collections import defaultdict
+
+
+def findCheapestPrice(
+    n: int, flights: list[list[int]], src: int, dst: int, K: int
+) -> int:
+    graph = defaultdict(list)
+    res = float("inf")
+    cola = [(src, 0, 0)]
+    for vuelo in flights:
+        graph[vuelo[0]].append((vuelo[1], vuelo[2]))
+    while cola:
+        viajeActual, nivel, precioActual = cola.pop(0)
+        if nivel > K + 1:
+            break
+        if viajeActual == dst:
+            res = min(res, precioActual)
+        for siguienteVuelo, precioSiguienteVuelo in graph[viajeActual]:
+            if precioSiguienteVuelo + precioActual >= res:
+                continue
+            cola.append(
+                (siguienteVuelo, nivel + 1, precioSiguienteVuelo + precioActual)
+            )
+    if res != float("inf"):
+        return res
+    else:
+        return -1
