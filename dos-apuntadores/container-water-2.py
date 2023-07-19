03608038e9456ebd72f5f171e9c4de02be873295
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-07-18 21:27:21 -0500

    Ejercicios de ventana deslizante y uno de apuntadores

--- dos-apuntadores/container-water-2.py
+++ dos-apuntadores/container-water-2.py
@@ -4,21 +4,18 @@
 # Este código se proporciona bajo la Licencia MIT.
 # Para más información, consulta el archivo LICENSE en la raíz del repositorio.
 
-def container(nums):
-  n=len(nums)
-  p1=0
-  p2=n-1
-  mejorArea=0
-  while p1<p2:
-    area=min(nums[p1], nums[p2])*(p2-p1)
-    if mejorArea<area:
-      mejorArea=area
-    if nums[p1]<=nums[p2]:
-       p1+=1
-    else:
-       p2-=1
-  return mejorArea
 
-if __name__=="__main__":
-    nums=list(map(int, input().split()))
-    print(container(nums))
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        n = len(height)
+        p1 = 0
+        p2 = n - 1
+        mejorArea = 0
+        while p1 < p2:
+            area = min(height[p1], height[p2]) * (p2 - p1)
+            mejorArea = max(mejorArea, area)
+            if height[p1] <= height[p2]:
+                p1 += 1
+            else:
+                p2 -= 1
+        return mejorArea
--- ventana-deslizante/minimum-window-substring.py
+++ ventana-deslizante/minimum-window-substring.py
@@ -1,3 +1,46 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        n = len(s)
+        m = len(t)
+        if m > n:
+            return ""
+        elif m == n:
+            return s if sorted(list(s)) == sorted(list(t)) else ""
+        else:
+            minimo = 10**5
+            res = ""
+            letras = {}
+            for j in range(m):
+                try:
+                    letras[t[j]] += 1
+                except:
+                    letras[t[j]] = 1
+            tiene = {}
+            comienza = 0
+            for i in range(n):
+                if s[i] in letras:
+                    if s[i] in tiene:
+                        tiene[s[i]] += 1
+                    else:
+                        tiene[s[i]] = 1
+                correcto = len(letras) == len(tiene)
+                if correcto:
+                    vamos = True
+                    for key in letras:
+                        if letras[key] > tiene[key]:
+                            vamos = False
+                            break
+                    if vamos:
+                        while vamos:
+                            comienza += 1
+                            if s[comienza - 1] in tiene:
+                                tiene[s[comienza - 1]] -= 1
+                            vamos = True
+                            for key in letras:
+                                if letras[key] > tiene[key]:
+                                    vamos = False
+                                    break
+                            if not vamos:
+                                if res == "" or len(res) > len(s[comienza - 1 : i + 1]):
+                                    res = s[comienza - 1 : i + 1]
+            return res
