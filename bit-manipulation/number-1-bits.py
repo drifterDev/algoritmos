463ac4c5af6aad96c2204a83f00c271ceeaabb0c
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-07-25 11:18:36 -0500

    Haciendo ejercicios con bits

---
+++ bit-manipulation/number-1-bits.py
@@ -0,0 +1,16 @@
+# Autor: Mateo Álvarez Murillo
+# Fecha de creación: 2023
+
+# Este código se proporciona bajo la Licencia MIT.
+# Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res=n
+        count=0
+        while res>1:
+            count+= 1 if res%2!=0 else 0
+            res=res//2
+        return count
+
