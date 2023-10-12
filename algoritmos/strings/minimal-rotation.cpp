68e200f9758b65233438d38c9447c4796d977cfe
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-11 22:16:15 -0500

    Minimal rotation

---
+++ algoritmos/strings/minimal-rotation.cpp
@@ -0,0 +1,35 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define len(str) ((int) str.length())
+
+// Encuentra la rotación lexicográficamente menor de un string
+int minExp(string &t) {
+  int i=0,j=1,k=0,n=len(t),x,y;
+  while(i<n && j<n && k<n) {
+    x=i+k;y=j+k;
+    if(x>=n)x-=n;
+    if(y>=n)y-=n;
+    if(t[x]==t[y])++k;
+    else if(t[x]>t[y]){
+      i=j+1>i+k+1?j+1:i+k+1;
+      swap(i,j);
+      k=0;
+    }else{
+      j=i+1>j+k+1?i+1:j+k+1;
+      k=0;
+    }
+  }
+  return i;
+}
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+return 0;
+}
