2393015cfc7682699e7ded021c7079e756d44aff
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-13 13:04:24 -0500

    LRS

--- algoritmos/strings/suffix-array.cpp
+++ algoritmos/strings/suffix-array.cpp
@@ -85,6 +85,20 @@ int different_substrings(string s) {
   return act;
 }
 
+string longest_repeated_substring(string& s){
+  vi sa=suffix_array(s);
+  vi lcp=lcp_construction(s,sa);
+  int n=len(s);
+  int max_len=0, start=0;
+  for(int i=0;i<n-1;i++){
+    if(lcp[i]>max_len){
+      max_len=lcp[i];
+      start=sa[i];
+    }
+  }
+  return s.substr(start,max_len);
+}
+
 // ******************************
 
 int main() {
