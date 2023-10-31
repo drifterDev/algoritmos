2cacc9fcd8f7abccbfeb446f7c009b77982aa551
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-31 13:07:23 -0500

    Contest de criba y de binary search

---
+++ contests/binary-search/c-interesting-drink.cpp
@@ -0,0 +1,35 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(arr) ((int) arr.size())
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9+7;
+int dirx[4] = {0,-1,1,0};
+int diry[4] = {-1,0,0,1};
+int dr[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+int n,queries, q;cin>>n;
+vi nums(n,0);for (int i=0;i<n;i++)cin>>nums[i];
+sort(nums.begin(), nums.end());cin>>queries;
+while(queries--){
+  cin>>q;
+  auto r=upper_bound(nums.begin(), nums.end(), q);
+  cout<<(r- nums.begin())<<"\n";
+}
+return 0;
+}
---
+++ contests/binary-search/d-sum-of-cubes.cpp
@@ -0,0 +1,35 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(arr) ((int) arr.size())
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+ll t,n,tope;cin>>t;
+double act;
+while(t--){
+  cin>>n;tope=cbrt(n);
+  bool pasa=false;
+  for(ll i=1;i<tope+1;i++){
+    act=cbrt(n-pow(i,3));
+    if(act == static_cast<int>(act) && pow(act, 3)+pow(i,3)==n && act!=0){
+      //cout<<act<<" "<<i<<"\n";
+      pasa=true;break;
+    }
+  }
+  if(pasa)cout<<"YES\n";
+  else cout<<"NO\n";
+}
+return 0;
+}
