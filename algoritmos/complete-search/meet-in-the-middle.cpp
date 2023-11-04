39398bfdb06d3db8760463ead2252577684479c8
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-11-04 11:39:57 -0500

    Meet in the middle

--- algoritmos/complete-search/meet-in-the-middle.cpp
+++ algoritmos/complete-search/meet-in-the-middle.cpp
@@ -6,19 +6,26 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define PB push_back
 typedef vector<int> vi;
 
 int main() {
 ios::sync_with_stdio(false);
 cin.tie(0);
-int n=3;
-vi nums={4,8,12};
+int n=4;
+vi nums={2,4,5,9};
+
+// Dividir en dos para poder reducir el 2^n a un 2^(n/2)
+n/=2;
 for(int b=0;b<(1<<n);b++){
-  vi subset;
-  for(int i=0;i<n;i++)if(b&(1<<i))subset.PB(i);
-  for(int x:subset)cout<<nums[x]<<" ";
-  cout<<"\n";
+  int suma=0;
+  for(int i=0;i<n;i++)if(b&(1<<i))suma+=nums[i];
+  cout<<suma<<" ";
+}cout<<"\n";
+
+for(int b=0;b<(1<<n);b++){
+  int suma=0;
+  for(int i=0;i<n;i++)if(b&(1<<i))suma+=nums[n+i];
+  cout<<suma<<" ";
 }
 return 0;
 }
