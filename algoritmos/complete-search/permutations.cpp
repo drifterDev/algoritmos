1d45150ac9238b266668a7386da0369f552764bc
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-11-04 10:39:12 -0500

    Subsets y permutaciones

---
+++ algoritmos/complete-search/permutations.cpp
@@ -0,0 +1,24 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+#define PB push_back
+typedef vector<int> vi;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+vi permutation;int n=3;
+for(int i=0;i<n;i++)permutation.PB(i);
+do {
+  cout<<"Permutation: ";
+  for(int i=0;i<n;++i)cout<<permutation[i]<<" ";
+  cout<<"\n";
+} while (next_permutation(all(permutation)));
+return 0;
+}
---
+++ algoritmos/complete-search/subsets.cpp
@@ -0,0 +1,24 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+typedef vector<int> vi;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(0);
+int n=3;
+vi nums={4,8,12};
+for (int b = 0; b < (1<<n); b++) {
+  vi subset;
+  for(int i=0;i<n;i++)if(b&(1<<i))subset.PB(i);
+  for(int x:subset)cout<<nums[x]<<" ";
+  cout<<"\n";
+}
+return 0;
+}
