286037f05e1868fda8f07b05ccfecadc7a313fe0
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-06 12:17:56 -0500

    Ecuaciones y primalidad

--- algoritmos/matematicas/primalidad.cpp
+++ algoritmos/matematicas/primalidad.cpp
@@ -8,10 +8,19 @@
 using namespace std;
 
 // O(sqrt(n))
-bool isPrime(int x){
-  for(int d=2;d*d<=x;d++)if(x%d==0)return false;
+bool isPrime(ll x){
+  for(ll d=2;d*d<=x;d++)if(x%d==0)return false;
   return x>=2;
 }
+
+// Con criba
+// O(sqrt(n)/log(sqrt(n)))
+bool isPrime(ll x){
+  if(n<criba_len)return criba;
+  for(int i=0;i<sz(p) && p[i]*p[i]<=n;++i)
+    if(n%p[i]==0)return false;
+  return true;  
+}  
 
 int main() {
 ios::sync_with_stdio(false);
