3acee72c5ae6b01a81de46cc57e5abd869fd7c0d
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-13 22:28:22 -0500

    Contest de disktra y prefix sum

--- contests/disktra/i-sending-email.cpp
+++ contests/disktra/i-sending-email.cpp
@@ -6,11 +6,28 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+#define watch(x) cout<<#x<<"="<<x<<'\n'
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int) x.size())
 #define PB push_back
 #define S second
 #define F first
 typedef long long ll;
 typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<string> vs;
+typedef vector<ii> vii;
+const double PI = acos(-1);
+const double EPS = 1e-9;
+const int MOD = 1e9+7;
+const int INF = 1e9;
+const ll INFL = 1e18;
+int dx4[4] = {0,-1,1,0};
+int dy4[4] = {-1,0,0,1};
+int dx8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dy8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const string ABC = "abcdefghijklmnopqrstuvwxyz";
 typedef pair<ll, ll> pll;
 typedef vector<pll> vpll;
 int n;
@@ -39,15 +56,20 @@ vl dijkstra(vector<vpll> &adj, int s){
 int main(){
   ios::sync_with_stdio(false);cin.tie(0);
   cout<<setprecision(20)<<fixed;
-  int m,s;
-  cin>>n>>m>>s;
-  vector<vpll> adj(n);
-  for(int i=0,a,b,w;i<m;++i){
-    cin>>a>>b>>w;
-    adj[a-1].PB({b-1,w});
-    adj[b-1].PB({a-1,w});
+  int q;cin>>q;
+  for(int i=1,m,s,t;i<=q;++i){
+    cin>>n>>m>>s>>t;
+    vector<vpll> adj(n, vpll());
+    ll a,b,w;
+    for(int i=0;i<m;++i){
+      cin>>a>>b>>w;
+      adj[a].PB({b,w});
+      adj[b].PB({a,w});
+    }
+    vl dist=dijkstra(adj,s);
+    cout<<"Case #"<<i<<": ";
+    if(dist[t]==LONG_LONG_MAX)cout<<"unreachable\n";
+    else cout<<dist[t]<<"\n";
   }
-  vl dist=dijkstra(adj,s-1);
-  for(int i=0;i<n;i++)cout<<dist[i]<<" ";
   return 0;
 }
