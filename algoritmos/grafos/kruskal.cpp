47b99aa782a39049a03410f7964c364e464d195e
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-27 13:42:34 -0500

    Prim y Kruskal

--- algoritmos/grafos/kruskal.cpp
+++ algoritmos/grafos/kruskal.cpp
@@ -6,8 +6,15 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+#define all(x) x.begin(), x.end()
+#define PB push_back
+#define S second
+#define F first
 typedef long long ll;
 typedef vector<ll> vl;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+int n,m;
 
 struct dsu{
   vl parents,sizes;
@@ -44,8 +51,32 @@ struct dsu{
   }
 };
 
+// O(mlogn)
+void kruskal(vector<pair<int,ii>>& edges){
+  sort(all(edges));
+  dsu uf(n);
+  ll res=0;
+  vii mst;
+  for(auto& e:edges){
+    if(!uf.isSameSet(e.S.F,e.S.S)){
+      uf.unionSets(e.S.F,e.S.S);
+      res+=e.F;
+      mst.PB(e.S);
+    }
+  }
+  for(auto& e:mst)cout<<e.F+1<<" "<<e.S+1<<"\n";
+  cout<<res<<"\n";
+}
+
 int main(){
   ios::sync_with_stdio(false);cin.tie(0);
   cout<<setprecision(20)<<fixed;
+  cin>>n>>m;
+  vector<pair<int,ii>> edges;
+  for(int i=0,u,v,w;i<m;++i){
+    cin>>u>>v>>w;
+    edges.PB({w,{u-1,v-1}});
+  }
+  kruskal(edges);
   return 0;
 }
---
+++ algoritmos/grafos/prim.cpp
@@ -0,0 +1,49 @@
+// Autor: Mateo Álvarez Murillo
+// Fecha de creación: 2023
+
+// Este código se proporciona bajo la Licencia MIT.
+// Para más información, consulta el archivo LICENSE en la raíz del repositorio.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define S second
+#define F first
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+int n,m;
+
+// O(n+mlogm)
+void prim(vector<vii>& edges){
+  priority_queue<ii,vii,greater<ii>> pq;
+  vector<bool> visited(n,false);
+  ll costo=0,count=0;
+  pq.push({0,0});
+  while(!pq.empty()){
+    ii curr=pq.top();pq.pop();
+    if(visited[curr.S])continue;
+    visited[curr.S]=true;
+    costo+=curr.F;
+    count++;
+    if(count==n)break;
+    for(auto& e:edges[curr.S]){
+      if(!visited[e.S])pq.push(e);
+    }
+  }
+  cout<<costo<<"\n";
+}
+
+int main(){
+  ios::sync_with_stdio(false);cin.tie(0);
+  cout<<setprecision(20)<<fixed;
+  cin>>n>>m;
+  vector<vii> edges(n);
+  for(int u,v,w,i=0;i<m;++i){
+    cin>>u>>v>>w;
+    edges[u-1].PB({w,v-1});
+    edges[v-1].PB({w,u-1});
+  }
+  prim(edges);
+  return 0;
+}
