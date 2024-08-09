021347d130cdb4b1cf3611ca2ad31a8d11fb021f
drifterDev <maalvarezmu@unal.edu.co>
2024-08-09 17:38:00 -0500

    Ordered set

--- estructuras/ordered-set.cpp
+++ estructuras/ordered-set.cpp
@@ -4,7 +4,8 @@ using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; 
+template<typename T> using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T> using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 // Para multiset cambiar less<int> por less_equal<int>
 // tree.find_by_order(k) -> Retorna un iterador al k-ésimo elemento 
 // tree.order_of_key(x) -> Retorna el número de elementos estrictamente menores a x
