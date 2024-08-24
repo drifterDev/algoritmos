7485ebd2a46ac68ecda80989ee8f4f364a491bdc
drifterDev <maalvarezmu@unal.edu.co>
2024-08-23 19:18:21 -0500

    RMQ

---
+++ estructuras/rmq.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long T;
+// max, min, gcd ...
+T oper(T a, T b){return max(a,b);}
+struct RMQ { // O(n)/O(1)
+    vector<vector<T>> table;
+    void build(vector<T>& v){
+        int n=v.size();
+        table.assign(20, vector<T>(n));
+        for(int i=0;i<n;++i)table[0][i]=v[i];
+        for(int j=1;(1<<j)<=n;++j)
+            for(int i=0;i+(1<<(j-1))<n;++i)
+                table[j][i]=oper(table[j-1][i],table[j-1][i+(1<<(j-1))]);
+    }
+    T get(int l, int r){
+        int j=31-__builtin_clz(r-l+1);
+        return oper(table[j][l], table[j][r-(1<<j)+1]);
+    }
+};
