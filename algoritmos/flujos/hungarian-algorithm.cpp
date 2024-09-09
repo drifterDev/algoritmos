02dd16c81822e4c8b95a4fd1e1622e587ffcf899
drifterDev <maalvarezmu@unal.edu.co>
2024-09-09 17:09:32 -0500

    Flujos

---
+++ algoritmos/flujos/hungarian-algorithm.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// task assignment O(n^3)
+template <class type>
+vector<int> hungarian(const vector<vector<type>>& cost){
+	const type inf=numeric_limits<type>::max();
+	int js=cost.size(); // jobs
+	int ws=cost[0].size(); // workers
+	vector<int> job(ws+1,-1); // -1 no assigned
+	vector<type> h(ws); 
+
+	for(int ji=0;ji<js;++ji){
+		int wi=ws;job[wi]=ji;
+		vector<type> dist(ws+1,inf);
+		dist[ws] = 0;
+		vector<bool> vis(ws+1); 
+		vector<int> prv(ws+1,-1); 
+		while(job[wi]!=-1){
+			type min_dist=inf;
+			vis[wi]=true;
+			int w_next=-1; 
+			for(int w=0;w<ws;++w){
+				if(!vis[w]){
+					type edge=cost[job[wi]][w]-h[w];
+					if(wi!=ws)edge-=cost[job[wi]][wi]-h[wi];
+					if(dist[w]>dist[wi]+edge){
+						dist[w]=dist[wi]+edge;
+						prv[w]=wi;
+					}
+					if(min_dist>dist[w]){
+						min_dist=dist[w];
+						w_next=w;
+					}
+				}
+			}
+			wi=w_next;
+		}
+		for(int w =0;w<ws;++w){ 
+			dist[w]=min(dist[w],dist[wi]);
+			h[w]+=dist[w];
+		}
+		while(wi!=ws){
+			job[wi]=job[prv[wi]];
+			wi=prv[wi];
+		}
+	}
+	return job;
+}
---
+++ algoritmos/flujos/maximum-flow.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(x) ((int) x.size())
+
+// O(m*n^2) - redes unitarias O(m*sqrt(n))
+template <class type> 
+struct network{
+	const type inf=numeric_limits<type>::max();
+	struct edge{int v;type cap;int inv;type flow;};
+	int n,s,t;
+	vector<int> lvl;
+	vector<vector<edge>> g;
+	network(int n):n(n),lvl(n),g(n){}
+	void add_edge(int u, int v, int c){
+		// edges.push_back({u,v});
+		g[u].push_back({v, c, sz(g[v]), 0});
+		g[v].push_back({u, 0, sz(g[u])-1, c});
+	}
+
+	bool bfs(){
+		fill(lvl.begin(),lvl.end(),-1);
+		queue<int> q;
+		lvl[s]=0;
+		for(q.push(s);q.size();q.pop()){
+			int u=q.front();
+			for(auto &e:g[u]) {
+				if(e.cap>0 && lvl[e.v]==-1){
+					lvl[e.v]=lvl[u]+1;
+					q.push(e.v);
+				}
+			}
+		}
+		return lvl[t]!=-1;
+	}
+
+	type dfs(int u, type nf){
+		if(u==t)return nf;
+		type res=0;
+		for(auto &e:g[u]){
+			if(e.cap>0 && lvl[e.v]==lvl[u]+1){
+				type tf=dfs(e.v, min(nf, e.cap));
+				res+=tf;nf-=tf;e.cap-=tf;
+				g[e.v][e.inv].cap+=tf;
+				g[e.v][e.inv].flow-=tf;
+				e.flow+=tf;
+				if(nf==0)return res;
+			}
+		}
+		if(!res)lvl[u]=-1;
+		return res;
+	}
+
+	type max_flow(int so, int si, type res=0){
+		s=so;t=si;
+		while(bfs())res+=dfs(s, inf);
+		return res;
+	}
+
+	vector<pair<int,int>> edges;
+	void min_cut(int so, int si){
+		cout<<max_flow(so, si)<<"\n";bfs();
+		for(auto x:edges){
+			if(lvl[x.first]!=-1 && lvl[x.second]==-1){
+				cout<<x.first+1<<" "<<x.second+1<<"\n";
+			}
+		}
+	}
+};
+
